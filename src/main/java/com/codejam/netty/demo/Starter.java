0c6aa81f8810b0b442c87a715bafe73a68c0f242
dell-zs <someone000@126.com>
2022-12-11 21:31:38 +0800

    ignore

--- src/main/java/com/codejam/netty/demo/Starter.java
+++ src/main/java/com/codejam/netty/demo/Starter.java
@@ -14,26 +14,33 @@ public class Starter {
 
         //1.输入流，输出流
         //2.RandomAccessFile
-        FileChannel   channel = new FileInputStream("D:\\Code\\my\\netty-demo1\\netty-demo1\\target\\classes\\data.txt").getChannel();
+        FileChannel channel = new FileInputStream("D:\\Code\\my\\netty-demo1\\netty-demo1\\target\\classes\\data.txt").getChannel();
 
         //channel = new FileInputStream("data.txt").getChannel();
         try {
 
 
-
             //准备缓冲区
             ByteBuffer buffer = ByteBuffer.allocate(100);
 
-            //从channel读取数据，向buffer写
-            int read = channel.read(buffer);
+            while (true) {
+                //从channel读取数据，向buffer写
+                int read = channel.read(buffer);
+                if (read == -1) {
+                    break;
+                }
 
-            //buffer内容打印
-            buffer.flip();  //切换至读模式
+                //buffer内容打印
+                buffer.flip();  //切换至读模式
 
-            while (buffer.hasRemaining()) {
-                byte b = buffer.get(); //一次一个字节
 
-                System.out.print((char) b);
+                while (buffer.hasRemaining()) {
+                    byte b = buffer.get(); //一次一个字节
+                    System.out.print((char) b);
+                }
+
+                //读完以后，buffer切换成写模式
+                buffer.clear();
             }
 
         } catch (Exception e) {
