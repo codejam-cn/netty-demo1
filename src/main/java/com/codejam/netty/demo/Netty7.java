13540b8c50935870ab07a0229275925de15007fc
dell-zs <someone000@126.com>
2022-12-18 22:54:24 +0800

    -

---
+++ src/main/java/com/codejam/netty/demo/Netty7.java
@@ -0,0 +1,26 @@
+package com.codejam.netty.demo;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+
+public class Netty7 {
+
+
+    /**
+     * 拆包和粘包
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * 网络上有多条数据，需要发给服务器， \n 分割
+         * （粘包、半包）
+         *
+         */
+
+    }
+}
